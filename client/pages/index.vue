18563d4282f6f415ad0d189fa4da95832a431114
Christian Gil <christian@chrisgvdev.com>
2022-05-11 20:05:34 -0500

    update: Implement initial layout for the hero section on the homepage

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,12 +2,24 @@
 
 <template>
   <NuxtLayout name="main">
-    <div class="flex h-screen w-full items-center justify-center bg-primary-900 text-white">
-      <Head>
-        <Title>Cryotainer LNG - Liquid Natural Gas</Title>
-      </Head>
+    <Head>
+      <Title>Cryotainer LNG - Liquid Natural Gas</Title>
+    </Head>
 
-      <h1 class="font-headline text-3xl font-bold">Welcome!</h1>
+    <div class="flex h-screen w-full items-center justify-center bg-primary-900">
+      <div class="flex flex-col items-center">
+        <div class="max-w-4xl">
+          <h1 class="text-center font-headline text-6xl font-light uppercase text-white">
+            Soluciones energéticas en <span class="font-bold">consumo de combustible</span>
+          </h1>
+        </div>
+
+        <NuxtLink
+          class="mt-8 inline-block rounded-md bg-primary-100 px-8 py-2 font-headline text-xs uppercase text-primary-900"
+          :to="{ name: 'nosotros' }"
+          >Sobre nosotros</NuxtLink
+        >
+      </div>
     </div>
   </NuxtLayout>
 </template>
