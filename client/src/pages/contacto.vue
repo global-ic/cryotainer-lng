afeb6abfccec3795bec0b16fcf0c648bcb0c253b
Christian Gil <christian@chrisgvdev.com>
2022-05-16 00:01:26 -0500

    update: Make the contact page responsice

--- client/src/pages/contacto.vue
+++ client/src/pages/contacto.vue
@@ -3,22 +3,22 @@ useHead({ title: 'Cryotainer LNG - Contáctanos' });
 </script>
 
 <template>
-  <div class="flex h-screen w-full gap-8 px-8 py-24 pb-8">
+  <div class="flex min-h-screen w-full flex-col-reverse gap-8 px-4 pt-24 pb-8 md:flex-row lg:px-8">
     <img
-      class="block h-full w-1/2 rounded-xl object-cover"
+      class="block aspect-video w-full rounded-xl object-cover md:aspect-auto md:w-1/2"
       src="https://images.pexels.com/photos/785529/pexels-photo-785529.jpeg?auto=compress&cs=tinysrgb&w=1000&dpr=1"
       alt=""
     />
 
     <div class="flex flex-1 flex-col items-center justify-center">
       <div class="max-w-sm text-center text-zinc-700">
-        <h1 class="font-headline text-6xl font-bold uppercase">Ponte en contacto</h1>
-        <span class="mt-4 block text-sm"
+        <h1 class="font-headline text-4xl font-bold uppercase md:text-6xl">Ponte en contacto</h1>
+        <span class="mt-2 block text-sm md:mt-4"
           >Si tienes algún proyecto o idea y requieres asesoría técnica no dudes en contactarnos.</span
         >
       </div>
 
-      <ul aria-label="Medios de contacto" class="mt-20 flex w-full max-w-sm flex-col space-y-4">
+      <ul aria-label="Medios de contacto" class="mt-12 flex w-full max-w-sm flex-col space-y-4 md:mt-20">
         <li class="method-container">
           <span class="method-name">Ubicación</span>
           <span class="method-value">Guadalajara, Jalisco, México</span>
