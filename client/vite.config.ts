09d4e0f33d9356407a7369a77c90e1b86d21e61a
Christian Gil <christian@chrisgvdev.com>
2022-06-08 11:06:27 -0500

    update(Client): Update the project

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,15 @@
 import Vue from '@vitejs/plugin-vue';
 import path from 'path';
 import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import Icons from 'unplugin-icons/vite';
 import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 import Inspect from 'vite-plugin-inspect';
 import Pages from 'vite-plugin-pages';
 import Layouts from 'vite-plugin-vue-layouts';
 import generateSitemap from 'vite-ssg-sitemap';
-import IconsResolver from 'unplugin-icons/resolver';
-import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
   resolve: {
@@ -25,7 +25,9 @@ export default defineConfig({
     Inspect(),
 
     AutoImport({
+      vueTemplate: true,
       dts: 'src/types/auto-imports.d.ts',
+      dirs: ['src/composables', 'src/stores'],
       imports: ['vue', 'vue-router', '@vueuse/head', '@vueuse/core'],
     }),
 
