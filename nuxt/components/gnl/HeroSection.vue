05630a81986655a8781dd9e6f72b65675b0eada5
Christian Gil <christian@chrisgvdev.com>
2022-07-15 23:29:54 -0500

    update(Client): Create the GNL hero section

---
+++ nuxt/components/gnl/HeroSection.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="pt-32">
+    <UiContainer>
+      <div class="mx-auto flex max-w-5xl flex-col items-center">
+        <img class="h-8 sm:h-10" src="/img/cryotainer-logo.png" alt="Logo Cryotainer LNG" />
+        <h1 class="mt-4 text-center font-headline text-4xl font-semibold uppercase text-zinc-900 md:text-6xl">
+          Gas Natural Licuado
+        </h1>
+        <p class="mt-6 max-w-3xl text-center text-lg text-zinc-500 md:text-xl">
+          Conoce más sobre el GNL. La mejor alternativa para el suministro de tanques vehiculares que es más
+          económico, seguro y ecológico.
+        </p>
+      </div>
+
+      <picture>
+        <source srcset="/img/webp/cryotainer-img-14.webp" type="image/webp" />
+        <source srcset="/img/cryotainer-img-14.jpg" type="image/jpeg" />
+        <img
+          src=""
+          alt="Tanque de GNL"
+          class="mt-16 max-h-[25rem] w-full rounded-xl object-cover sm:h-[50vh]"
+        />
+      </picture>
+    </UiContainer>
+  </section>
+</template>
--- nuxt/pages/gnl.vue
+++ nuxt/pages/gnl.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 useHead({ title: 'Gas Natural Licuado' });
+definePageMeta({ header: { color: 'dark' } });
 </script>
 
 <template>
-  <div class="flex h-screen w-full items-center justify-center bg-primary-900 text-white">
-    <h1 class="font-headline text-3xl font-bold">GNL</h1>
-  </div>
+  <main>
+    <GnlHeroSection />
+  </main>
 </template>
