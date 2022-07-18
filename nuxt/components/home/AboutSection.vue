78ceb32fa21a02224fcba8e679e2276a9d6ece2d
Christian Gil <christian@chrisgvdev.com>
2022-07-18 16:27:50 -0500

    update(Client): Improve the about section of the home page

--- nuxt/components/home/AboutSection.vue
+++ nuxt/components/home/AboutSection.vue
@@ -1,26 +1,36 @@
-<template>
-  <section aria-label="Nosotros">
-    <div
-      class="mx-auto flex h-screen w-full max-w-lg flex-col items-center justify-center gap-4 px-4 md:gap-8 lg:px-8"
-    >
-      <h2
-        class="text-center font-headline text-3xl font-bold uppercase text-zinc-800 md:text-4xl lg:text-5xl"
-      >
-        Especialistas en soluciones de Gas Natural
-      </h2>
+<script setup lang="ts"></script>
 
-      <div class="space-y-4 text-center text-lg text-zinc-700">
-        <p>
+<template>
+  <section class="py-24">
+    <UiContainer>
+      <div class="flex flex-col gap-8 md:flex-row lg:gap-16">
+        <div class="w-full md:w-1/2 xl:w-full xl:max-w-3xl">
+          <span class="mini-title">Sobre Cryotainer LNG</span>
+          <h2 class="mt-3 font-headline text-4xl font-semibold uppercase text-zinc-900 lg:text-5xl">
+            Proveedores de equipos y servicios de primer nivel
+          </h2>
+
+          <div class="mt-8 flex gap-2">
+            <NuxtLink class="btn btn-primary" :to="{ name: 'contacto' }">Contáctenos</NuxtLink>
+            <NuxtLink class="btn btn-outlined" :to="{ name: 'nosotros' }">Más información</NuxtLink>
+          </div>
+        </div>
+
+        <p class="flex-1 text-lg text-zinc-500 sm:text-xl">
           En Cryotainer LNG somos empresa 100% mexicana donde brindamos la más alta calidad para la entrega y
           distribución de gas natural de forma eficiente, profesional segura y sobre todo ecológica.
         </p>
-        <p>
-          Contamos con especialistas calificados para ofrecer soluciones personalizadas para cada sector, sea
-          minería, invernaderos, transporte, energía eléctrica, etc.
-        </p>
       </div>
 
-      <NuxtLink class="btn btn-outlined" :to="{ name: 'contacto' }">Contáctanos</NuxtLink>
-    </div>
+      <picture>
+        <source srcset="/img/webp/cryotainer-img-25.webp" type="image/webp" />
+        <source srcset="/img/cryotainer-img-25.jpg" type="image/jpeg" />
+        <img
+          src=""
+          alt="Camiones siendo cargados con GNL"
+          class="mt-16 max-h-[30rem] w-full rounded-xl object-cover sm:h-[60vh]"
+        />
+      </picture>
+    </UiContainer>
   </section>
 </template>
