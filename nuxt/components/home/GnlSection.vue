18a32ba6fccb462d0f9c345673e6aba654474a52
Christian Gil <christian@chrisgvdev.com>
2022-07-18 16:48:02 -0500

    update(Client): Improve the GNL section on the home page

--- nuxt/components/home/GnlSection.vue
+++ nuxt/components/home/GnlSection.vue
@@ -1,23 +1,72 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MoneyIcon from '~icons/ph/money';
+import ShieldCheckIcon from '~icons/ph/shield-check';
+import TreeIcon from '~icons/ph/tree';
+
+const features = [
+  {
+    icon: MoneyIcon,
+    title: 'Económico',
+    body: 'El proceso de licuefacción condensa el volumen de gas hasta 600 veces, lo que permite un transporte más fácil a áreas donde no existen gasoductos de gas natural.',
+  },
+  {
+    icon: ShieldCheckIcon,
+    title: 'Seguro',
+    body: 'El GNL es un gas natural no tóxico, no corrosivo e inodoro, lo que reduce los riesgos de su manipulación y lo vuelve en una alternativa más segura.',
+  },
+  {
+    icon: TreeIcon,
+    title: 'Ecológico',
+    body: 'Distinguido como uno de los combustibles fósiles más limpios disponibles, el GNL es una alternativa confiable y respetuosa con el medio ambiente al gas natural.',
+  },
+];
+</script>
 
 <template>
-  <section aria-label="Gas Natural Licuado" class="h-[70vh] p-4 md:h-screen lg:p-8">
-    <div
-      style="background-image: url(/img/cryotainer-gnl-ship.jpg)"
-      class="relative flex h-full w-full items-center justify-center overflow-hidden rounded-xl bg-cover bg-center p-4 lg:p-8"
-    >
-      <div class="pointer-events-none absolute inset-0 z-0 bg-zinc-900 opacity-50"></div>
-
-      <div class="z-10 w-full max-w-xl space-y-6 text-center md:space-y-8">
-        <h2 class="font-headline text-2xl font-bold uppercase text-white md:text-4xl lg:text-5xl">
-          Gas Natural Licuado (GNL)
-        </h2>
-        <p class="text-lg text-zinc-100">
-          Le demanda de gas natural está creciendo constantemente, dando oportunidad a un cambio ecológico
-          para las energías en México… El Gas Natural Licuado (GNL).
+  <section class="py-24">
+    <UiContainer>
+      <div class="max-w-3xl">
+        <h2 class="mini-title">Gas Natural Licuado (GNL)</h2>
+        <span class="mt-3 font-headline text-4xl font-semibold uppercase text-zinc-900"
+          >Contamos con la mejor alternativa al GNC y Diesel</span
+        >
+        <p class="mt-5 text-lg text-zinc-500 md:text-xl">
+          El gas natural licuado, o GNL, es un tipo de gas natural no tóxico, no corrosivo e inodoro. Durante
+          la licuefacción, el GNL se obtiene enfriando el gas a -162° (-260°F).
         </p>
-        <NuxtLink class="btn btn-outlined-white" :to="{ name: 'gnl' }">Aprende más</NuxtLink>
+
+        <NuxtLink class="btn btn-primary mt-8" :to="{ name: 'gnl' }">Más información</NuxtLink>
       </div>
-    </div>
+
+      <div class="mt-6 grid items-center gap-x-16 gap-y-8 md:mt-8 md:grid-cols-2">
+        <ul class="flex flex-col space-y-6 md:space-y-12">
+          <li v-for="feature in features" class="flex flex-col xs:flex-row xs:space-x-4">
+            <div
+              aria-hidden="true"
+              class="flex h-12 w-12 items-center justify-center rounded-full border-8 border-primary-50 bg-primary-100 text-primary-600"
+            >
+              <component :is="feature.icon" class="h-6 w-6" />
+            </div>
+
+            <div class="mt-2 flex-1 space-y-2">
+              <h4 class="font-headline text-xl font-semibold text-zinc-900">{{ feature.title }}</h4>
+              <p class="mt-2 text-zinc-500">{{ feature.body }}</p>
+            </div>
+          </li>
+        </ul>
+
+        <div class="relative h-80 md:h-[35rem] lg:h-[80vh]">
+          <picture>
+            <source srcset="/img/webp/cryotainer-img-14.webp" type="image/webp" />
+            <source srcset="/img/cryotainer-img-14.jpg" type="image/jpeg" />
+            <img
+              src=""
+              alt="Camión con tanque de GNL"
+              class="absolute h-full w-full rounded-xl object-cover md:left-[10%] lg:w-[50vw] lg:max-w-none"
+            />
+          </picture>
+        </div>
+      </div>
+    </UiContainer>
   </section>
 </template>
