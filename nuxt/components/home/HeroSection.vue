76d56d161727b7ded6d88762f81be915172e4bd4
Christian Gil <christian@chrisgvdev.com>
2022-07-18 16:22:12 -0500

    update(Client): Update the hero section

--- nuxt/components/home/HeroSection.vue
+++ nuxt/components/home/HeroSection.vue
@@ -3,17 +3,9 @@ import { gsap } from 'gsap';
 
 const line1 = ref<HTMLSpanElement>();
 const line2 = ref<HTMLSpanElement>();
-const heroSection = ref<HTMLElement>();
 
 onMounted(() => {
   const tl = gsap.timeline();
-
-  tl.to(heroSection.value!, {
-    opacity: 1,
-    delay: 0.5,
-    duration: 1,
-    ease: 'power2',
-  });
 
   tl.fromTo(
     [line1.value, line2.value],
@@ -27,38 +19,42 @@ onMounted(() => {
       duration: 1,
       stagger: 0.1,
       ease: 'power2',
-    },
-    '-=0.75'
+    }
   );
 
-  tl.to('.hero-link', { opacity: 1, duration: 0.5, ease: 'power2' });
+  tl.from('.hero-link', { opacity: 0, duration: 0.5, ease: 'power2' });
 });
 </script>
 
 <template>
-  <section
-    ref="heroSection"
-    aria-label="Portada"
-    style="background-image: url(/img/cryotainer-hero.jpg)"
-    class="flex h-screen w-full transform items-center justify-center bg-zinc-900/50 bg-cover bg-center opacity-0"
-  >
+  <section aria-label="Portada" class="relative flex h-screen w-full items-center justify-center">
+    <div aria-hidden="true" class="absolute inset-0 -z-10">
+      <div class="relative h-full w-full overflow-hidden">
+        <div class="absolute inset-0 bg-zinc-900 bg-opacity-50" />
+
+        <picture>
+          <source srcset="/img/webp/cryotainer-img-23.webp" type="image/webp" />
+          <source srcset="/img/cryotainer-img-23.jpg" type="image/jpeg" />
+          <img src="" alt="" class="h-full w-full object-cover object-bottom" />
+        </picture>
+      </div>
+    </div>
+
     <div class="flex flex-col items-center px-4 lg:px-8">
       <div class="max-w-4xl">
         <h1
           class="text-center font-headline text-4xl font-light uppercase text-white md:text-5xl lg:text-6xl"
         >
           <div class="overflow-hidden">
-            <span ref="line1" class="block">Soluciones energéticas en</span>
+            <span ref="line1" class="block">Especialistas en</span>
           </div>
           <div class="overflow-hidden">
-            <span ref="line2" class="block font-bold">consumo de combustible</span>
+            <span ref="line2" class="block font-bold">equipo criogénico</span>
           </div>
         </h1>
       </div>
 
-      <NuxtLink class="hero-link btn btn-primary mt-8 opacity-0" :to="{ name: 'nosotros' }"
-        >Sobre nosotros</NuxtLink
-      >
+      <NuxtLink class="hero-link btn btn-primary mt-8" :to="{ name: 'nosotros' }">Sobre nosotros</NuxtLink>
     </div>
   </section>
 </template>
