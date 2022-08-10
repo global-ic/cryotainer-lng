6bde8f7b5d70fe5d3ebbb903a674d98e635ddcdc
Christian Gil <christian@chrisgvdev.com>
2022-08-10 14:35:56 -0500

    update: Add animation to the gnl TransportSection

--- components/gnl/TransportSection.vue
+++ components/gnl/TransportSection.vue
@@ -1,4 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { gsap } from 'gsap';
+
+const imgCover = ref<HTMLElement>();
+const mainImg = ref<HTMLImageElement>();
+
+onMounted(() => {
+  const tl = gsap.timeline({ scrollTrigger: { trigger: mainImg.value, start: 'top 90%' } });
+  tl.to(imgCover.value, {
+    width: 0,
+    duration: 1.5,
+    ease: 'expo.inOut',
+  });
+
+  tl.from(mainImg.value, { scale: 1.5, duration: 2, opacity: 0, ease: 'expo' }, '<0.5');
+});
+</script>
 
 <template>
   <section class="py-24">
@@ -17,15 +33,20 @@
         </p>
       </div>
 
-      <picture>
-        <source srcset="/img/webp/cryotainer-img-16.webp" type="image/webp" />
-        <source srcset="/img/cryotainer-img-16.jpg" type="image/jpeg" />
-        <img
-          src=""
-          alt="Camiones siendo cargados con GNL"
-          class="mt-16 max-h-[30rem] w-full rounded-xl object-cover sm:h-[60vh]"
-        />
-      </picture>
+      <div class="relative mt-16 overflow-hidden rounded-xl">
+        <picture>
+          <source srcset="/img/webp/cryotainer-img-16.webp" type="image/webp" />
+          <source srcset="/img/cryotainer-img-16.jpg" type="image/jpeg" />
+          <img
+            src=""
+            ref="mainImg"
+            alt="Camiones siendo cargados con GNL"
+            class="max-h-[30rem] w-full origin-center transform object-cover sm:h-[60vh]"
+          />
+        </picture>
+
+        <div ref="imgCover" class="absolute top-0 left-0 h-full w-full origin-left bg-gray-50"></div>
+      </div>
     </UiContainer>
   </section>
 </template>
