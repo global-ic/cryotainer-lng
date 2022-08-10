451d747b3725ecbbf14b1a9641323647e9378a13
Christian Gil <christian@chrisgvdev.com>
2022-08-10 14:45:03 -0500

    update: Add animation to the gnl FillingSection

--- components/gnl/FillingSection.vue
+++ components/gnl/FillingSection.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts">
+import { gsap } from 'gsap';
 import GasPumpIcon from '~icons/ph/gas-pump';
+
+const imgCover = ref<HTMLElement>();
+const mainImg = ref<HTMLImageElement>();
+const cards = ref<HTMLElement[]>([]);
+const cardsWrapper = ref<HTMLElement>();
+
+onMounted(() => {
+  const tl = gsap.timeline({ scrollTrigger: { trigger: mainImg.value, start: 'top 90%' } });
+  tl.to(imgCover.value, {
+    scaleX: 0,
+    duration: 2,
+    ease: 'expo.inOut',
+  });
+
+  tl.from(mainImg.value, { scale: 1.5, duration: 2, opacity: 0, ease: 'expo' }, '<0.5');
+});
 </script>
 
 <template>
@@ -29,15 +46,25 @@ import GasPumpIcon from '~icons/ph/gas-pump';
       </div>
 
       <div class="relative h-80 md:h-[35rem] lg:h-[80vh]">
-        <picture>
-          <source srcset="/img/webp/cryotainer-img-18.webp" type="image/webp" />
-          <source srcset="/img/cryotainer-img-18.jpg" type="image/jpeg" />
-          <img
-            src=""
-            alt="Tanque de GNL - Cryotainer LNG"
-            class="absolute h-full w-full rounded-xl object-cover md:left-[10%] md:rounded-b-none lg:w-[50vw] lg:max-w-none"
-          />
-        </picture>
+        <div
+          class="absolute h-full w-full overflow-hidden rounded-xl md:left-[10%] md:rounded-b-none lg:w-[50vw]"
+        >
+          <picture>
+            <source srcset="/img/webp/cryotainer-img-18.webp" type="image/webp" />
+            <source srcset="/img/cryotainer-img-18.jpg" type="image/jpeg" />
+            <img
+              src=""
+              ref="mainImg"
+              alt="Tanque de GNL por Cryotainer"
+              class="h-full w-full origin-center transform object-cover lg:max-w-none"
+            />
+          </picture>
+        </div>
+
+        <div
+          ref="imgCover"
+          class="absolute top-0 left-0 h-full w-full origin-right bg-gray-50 md:left-[10%] lg:w-[50vw]"
+        ></div>
       </div>
     </UiContainer>
   </section>
