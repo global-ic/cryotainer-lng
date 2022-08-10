c9f87edb09e5342fb86ec5d0f6d707ed8fdc3fde
Christian Gil <christian@chrisgvdev.com>
2022-08-10 12:14:52 -0500

    update: Add animations to the home GnlSection

--- components/home/GnlSection.vue
+++ components/home/GnlSection.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { gsap } from 'gsap';
 import MoneyIcon from '~icons/ph/money';
 import ShieldCheckIcon from '~icons/ph/shield-check';
 import TreeIcon from '~icons/ph/tree';
@@ -20,6 +21,31 @@ const features = [
     body: 'Distinguido como uno de los combustibles fósiles más limpios disponibles, el GNL es una alternativa confiable y respetuosa con el medio ambiente al gas natural.',
   },
 ];
+
+const imgCover = ref<HTMLElement>();
+const mainImg = ref<HTMLImageElement>();
+const cards = ref<HTMLElement[]>([]);
+const cardsWrapper = ref<HTMLElement>();
+
+onMounted(() => {
+  gsap.from(cards.value, {
+    y: 50,
+    opacity: 0,
+    duration: 1.5,
+    ease: 'expo',
+    scrollTrigger: { trigger: cardsWrapper.value, start: 'top 70%' },
+    stagger: 0.3,
+  });
+
+  const tl = gsap.timeline({ scrollTrigger: { trigger: mainImg.value, start: 'top 90%' } });
+  tl.to(imgCover.value, {
+    scaleX: 0,
+    duration: 2,
+    ease: 'expo.inOut',
+  });
+
+  tl.from(mainImg.value, { scale: 1.5, duration: 2, opacity: 0, ease: 'expo' }, '<0.5');
+});
 </script>
 
 <template>
@@ -38,9 +64,9 @@ const features = [
         <NuxtLink class="btn btn-primary mt-8" :to="{ name: 'gnl' }">Más información</NuxtLink>
       </div>
 
-      <div class="mt-6 grid items-center gap-x-16 gap-y-8 md:mt-8 md:grid-cols-2">
+      <div ref="cardsWrapper" class="mt-6 grid items-center gap-x-16 gap-y-8 md:mt-8 md:grid-cols-2">
         <ul class="flex flex-col space-y-6 md:space-y-12">
-          <li v-for="feature in features" class="flex flex-col xs:flex-row xs:space-x-4">
+          <li ref="cards" v-for="feature in features" class="flex flex-col xs:flex-row xs:space-x-4">
             <div
               aria-hidden="true"
               class="flex h-12 w-12 items-center justify-center rounded-full border-8 border-primary-50 bg-primary-100 text-primary-600"
@@ -56,15 +82,23 @@ const features = [
         </ul>
 
         <div class="relative h-80 md:h-[35rem] lg:h-[80vh]">
-          <picture>
-            <source srcset="/img/webp/cryotainer-img-14.webp" type="image/webp" />
-            <source srcset="/img/cryotainer-img-14.jpg" type="image/jpeg" />
-            <img
-              src=""
-              alt="Camión con tanque de GNL"
-              class="absolute h-full w-full rounded-xl object-cover md:left-[10%] lg:w-[50vw] lg:max-w-none"
-            />
-          </picture>
+          <div class="absolute h-full w-full overflow-hidden rounded-xl md:left-[10%] lg:w-[50vw]">
+            <picture>
+              <source srcset="/img/webp/cryotainer-img-14.webp" type="image/webp" />
+              <source srcset="/img/cryotainer-img-14.jpg" type="image/jpeg" />
+              <img
+                src=""
+                ref="mainImg"
+                alt="Camión con tanque GNL"
+                class="h-full w-full origin-center transform object-cover lg:max-w-none"
+              />
+            </picture>
+          </div>
+
+          <div
+            ref="imgCover"
+            class="absolute top-0 left-0 h-full w-full origin-right bg-gray-50 md:left-[10%] lg:w-[50vw]"
+          ></div>
         </div>
       </div>
     </UiContainer>
