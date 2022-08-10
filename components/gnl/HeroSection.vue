792b9365af2e71444cad56884e5a097dc08ae9fa
Christian Gil <christian@chrisgvdev.com>
2022-08-10 14:32:27 -0500

    update: Add animation to the gnl HeroSection

--- components/gnl/HeroSection.vue
+++ components/gnl/HeroSection.vue
@@ -1,4 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { gsap } from 'gsap';
+
+const imgCover = ref<HTMLElement>();
+const headline = ref<HTMLElement>();
+const supporting = ref<HTMLElement>();
+const mainImg = ref<HTMLImageElement>();
+
+onMounted(() => {
+  const tl = gsap.timeline();
+
+  tl.from([headline.value, supporting.value], {
+    y: 50,
+    opacity: 0,
+    duration: 1.75,
+    ease: 'expo.inOut',
+    stagger: 0.1,
+  });
+
+  tl.to(
+    imgCover.value,
+    {
+      height: 0,
+      duration: 1.5,
+      ease: 'expo.inOut',
+    },
+    '>-1'
+  );
+
+  tl.from(mainImg.value, { scale: 1.5, duration: 2, opacity: 0, ease: 'expo' }, '<0.5');
+});
+</script>
 
 <template>
   <section class="pt-32">
@@ -10,23 +41,31 @@
           <img class="h-8 sm:h-10" src="" alt="Logo Cryotainer LNG" />
         </picture>
 
-        <h1 class="mt-4 text-center font-headline text-4xl font-semibold uppercase text-zinc-900 md:text-6xl">
+        <h1
+          ref="headline"
+          class="mt-4 text-center font-headline text-4xl font-semibold uppercase text-zinc-900 md:text-6xl"
+        >
           Gas Natural Licuado
         </h1>
-        <p class="mt-6 max-w-3xl text-center text-lg text-zinc-500 md:text-xl">
+        <p ref="supporting" class="mt-6 max-w-3xl text-center text-lg text-zinc-500 md:text-xl">
           El mejor combustible para recorrer largas distancias siendo mas económico, seguro y ecológico.
         </p>
       </div>
 
-      <picture>
-        <source srcset="/img/webp/cryotainer-img-14.webp" type="image/webp" />
-        <source srcset="/img/cryotainer-img-14.jpg" type="image/jpeg" />
-        <img
-          src=""
-          alt="Tanque de GNL"
-          class="mt-16 max-h-[25rem] w-full rounded-xl object-cover sm:h-[50vh]"
-        />
-      </picture>
+      <div class="relative mt-16 overflow-hidden rounded-xl">
+        <picture>
+          <source srcset="/img/webp/cryotainer-img-14.webp" type="image/webp" />
+          <source srcset="/img/cryotainer-img-14.jpg" type="image/jpeg" />
+          <img
+            src=""
+            ref="mainImg"
+            alt="Camiones siendo cargados con GNL"
+            class="max-h-[25rem] w-full origin-center transform object-cover sm:h-[50vh]"
+          />
+        </picture>
+
+        <div ref="imgCover" class="absolute top-0 left-0 h-full w-full origin-left bg-gray-50"></div>
+      </div>
     </UiContainer>
   </section>
 </template>
